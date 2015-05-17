7ad2f568d4ab854845c4fc3e2e54f4f67ae96931
GreyKnight <tinyplasticgreyknight@yahoo.com>
2015-05-17 16:23:52 +0100

    pull some constants out

--- make-rest.py
+++ make-rest.py
@@ -6,16 +6,20 @@ from gather import *
 from funcs import *
 from verify import *
 
+MAIN_REPO_ROOT = "../modern-data"
+CONTENT_DIR = "content"
+SPHINX_CONF = "sphinx-conf.py"
+
 def main(source_dir):
 	root = gather_docs()
 	for child in root.children:
 		verify_consistency(child)
 	root.create_tree(source_dir)
 	ensure_dir_exists(os.path.join(source_dir, "_static"))
-	shutil.copy("sphinx-conf.py", os.path.join(source_dir, "conf.py"))
+	shutil.copy(SPHINX_CONF, os.path.join(source_dir, "conf.py"))
 
 def gather_docs():
-	root = gather_directory(None, ".", "content")
+	root = gather_directory(None, ".", CONTENT_DIR)
 	root.name = None
 	root.is_root = True
 	#root.toc_depth = 2
